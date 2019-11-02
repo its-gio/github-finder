1fe359d83454a2b376362474f36403b37ad341a9
Giovani Aguirre <me@its-g.io>
2019-11-01 18:02:14 -0700

    Creating a Promise.all for both git fetch requests

--- js/index.js
+++ js/index.js
@@ -3,9 +3,13 @@ import { ID , SECRET } from "./keys.js";
 class Github {
   // promise.all([arrayOf, fetches])
   getInfo(user) {
-    fetch(`https://api.github.com/users/its-gio?client_id=${ID}&client_secret=${SECRET}`)
-      .then(blob => blob.json())
-      .then(data => console.log(data));
+    let milk = fetch(`https://api.github.com/users/its-gio?client_id=${ID}&client_secret=${SECRET}`)
+      .then(blob => blob.json());
+    let shake = fetch(`https://api.github.com/users/its-gio/repos?client_id=${ID}&client_secret=${SECRET}`)
+      .then(blob => blob.json());
+    
+    Promise.all([milk, shake])
+      .then(([data1, data2]) => console.log(data1, data2));
   }
 }
 
