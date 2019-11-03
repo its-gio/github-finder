2e5eb7a889bcf2bc9508475070397dac28631b02
Giovani Aguirre <me@its-g.io>
2019-11-02 17:56:03 -0700

    Moved promise outside of class object

--- js/index.js
+++ js/index.js
@@ -1,20 +1,21 @@
 import { ID , SECRET } from "./keys.js";
 
+document.querySelector("form").addEventListener("submit", (e) => e.preventDefault());
+
 class Github {
-  // promise.all([arrayOf, fetches])
-  getInfo(user) {
-    let milk = fetch(`https://api.github.com/users/its-gio?client_id=${ID}&client_secret=${SECRET}`)
+  getInfo(userTyped) {
+    const milk = fetch(`https://api.github.com/users/its-gio?client_id=${ID}&client_secret=${SECRET}`)
       .then(blob => blob.json());
-    let shake = fetch(`https://api.github.com/users/its-gio/repos?client_id=${ID}&client_secret=${SECRET}`)
+    const shake = fetch(`https://api.github.com/users/its-gio/repos?client_id=${ID}&client_secret=${SECRET}`)
       .then(blob => blob.json());
     
-    Promise.all([milk, shake])
-      .then(([data1, data2]) => console.log(data1, data2));
+    return Promise.all([milk, shake]);
   }
 }
 
 document.getElementById("name").addEventListener("keyup", (e) => {
   const GH = new Github;
-
-  GH.getInfo(e.target.value);
+  
+  GH.getInfo(e.target.value)
+    .then(([user, repos]) => [user, repos]);
 })
