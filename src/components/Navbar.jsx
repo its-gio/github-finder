80545b641eaed01aed7448d588bacdc342a69c60
Giovani Aguirre <me@its-g.io>
2020-02-16 20:56:02 -0600

    Changed name of h1 to github finder.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 export default class Navbar extends Component {
   render() {
     return (
-      <div>
-        <h1>Navbar</h1>
-      </div>
+      <nav>
+        <h1>Github Finder</h1>
+      </nav>
     )
   }
 }
