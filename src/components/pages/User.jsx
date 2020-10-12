c8300ed85aae7d4a7e5db002421d4e0c7ed38fe2
Giovani Aguirre <me@its-g.io>
2020-10-12 16:05:55 -0700

    Got info from single user and displayed on page with light styles.

--- src/components/pages/User.jsx
+++ src/components/pages/User.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import Spinner from "../Spinner";
+import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
 
 function User(props) {
   useEffect(() => {
@@ -8,6 +9,7 @@ function User(props) {
 
   const {
     name,
+    company,
     avatar_url,
     location,
     bio,
@@ -17,13 +19,45 @@ function User(props) {
     public_repos,
     public_gists,
     followers,
-    followering,
+    following,
     hireable,
   } = props.user;
 
   if (props.loading) return <Spinner />
 
-  return <div>{name}</div>;
-}
+  return (
+    <div className="single-user">
+      <p className="single-user--hireable">
+        <strong>Hirable:</strong>
+        { hireable ? <FaCheckCircle fill="green" /> : <FaTimesCircle fill="red" /> }
+      </p>
+
+      <div className="single-user--info">
+        <div className="single-user--info__personal">
+          <div className="avatar-container">
+            <img src={avatar_url} alt={`${name}'s github avatar`}/>
+          </div>
+          <h3>{name}</h3>
+          <p><strong>Location:</strong> {location}</p>
+          <p><strong>Public Repos:</strong> {public_repos}</p>
+          <p><strong>Public Repos:</strong> {public_gists}</p>
+          <p><strong>Followers:</strong> {followers}</p>
+          <p><strong>Followering:</strong> {following}</p>
+        </div>
+
+        <div className="single-user--info__connect">
+          { bio && <p><strong>Bio:</strong><br/>{bio}</p> }
+
+          { html_url && <a href={html_url} className="user-website">Github Profile</a> }
+
+          { login && <p><strong>Username:</strong> {login}</p> }
+
+          { company && <p><strong>Company:</strong> {company}</p> }
+
+          { blog && <p><strong>Website:</strong> <a href={`http://${blog}`}>{blog}</a></p> }
+        </div>
+      </div>
+    </div>
+  )}
 
 export default User;
